1656c4a4f3805e47ccd08ef6dabec948a2245489
deverex <31496483+deverex@users.noreply.github.com>
2019-06-08 08:42:08 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+var passport = require('passport');
+var authenticate = require('../authenticate');
+const cors = require('./cors');
+
+var User = require('../models/user');
+
+router.use(bodyParser.json());
+
+/* GET users listing. */
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({}, (err,users) => {
+    if(err){
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ err: err });
+    }
+    res.json(users);
+  });
+});
+
+router.post('/signup', cors.corsWithOptions, (req,res,next) => {
+  User.register(new User({username:req.body.username}), 
+  req.body.password, (err,user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type','application/json');
+      res.json({err:err});
+    }
+    else{
+      if(req.body.firstname)
+      user.firstname = req.body.firstname;
+      if(req.body.lastname)
+      user.lastname = req.body.lastname;
+      user.save((err,user) => {
+        if(err){
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ err: err });
+          return;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req,res) => {
+  var token = authenticate.getToken({_id:req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type','application/json');
+  res.json({success:true , token:token , status:'You are logged in!'});
+});
+
+router.get('/logout', cors.corsWithOptions, (req,res) => {
+  if(req.session){
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else{
+    var err = new Error('You are not authenticated');
+    err.status = 403;
+    next(err);
+  }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req,res) => {
+  if(req.user) {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token: token, status: 'You are logged in!' });
+  }
+});
+
+module.exports = router;
